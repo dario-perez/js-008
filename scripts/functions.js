915c0efec422dc84279dbc31161201bf7bffd0b1
Dario Perez <darioperez.2105@gmail.com>
2023-02-12 06:22:26 -0300

    function correction

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,7 +3,14 @@ function trueMachine() {
 }
 
 function asyncFunc() {
-  setTimeout(() => {
+  return setTimeout(() => {
     console.log('Hola, soy una promesa');
   }, 5000);
+}
+
+function* idsPares() {
+  let id = 0;
+  while (true) {
+    yield (id += 2);
+  }
 }
